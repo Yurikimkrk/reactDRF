7525a986cda6462c718cfff17b5a28d9bcd5e2db
yuri <yurikimkrk@gmail.com>
2021-07-18 10:27:03 +0700

    start mini-project

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import {Component} from "react"
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'authors': []
+    }
+  }
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+
+  }
+}
+
+export default App
